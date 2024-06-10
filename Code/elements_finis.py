5c5a90dec290ef1908622b6d54a4d54be289c911
Collette Jean-Luc <jean-luc.collette@centralesupelec.fr>
2024-06-10 08:07:37 +0000

    Replace elements_finis.py

--- Code/elements_finis.py
+++ Code/elements_finis.py
@@ -3,9 +3,9 @@ from scipy.sparse import csr_array
 from scipy.sparse.linalg import spsolve
 
 
-def coeffDM(trs, pts):
-    liste_coeffD = np.zeros((trs.shape[0], 3, 3))
-    liste_coeffM = np.zeros((trs.shape[0], 3, 3))
+def coeffDK(trs, pts):
+    liste_Dp = np.zeros((trs.shape[0], 3, 3))
+    liste_Kp = np.zeros((trs.shape[0]))
     Dphi = np.array([[-1, -1], [1, 0], [0, 1]])
     for tr in range(trs.shape[0]):
         xp = pts[trs[tr, :], 0]
@@ -14,16 +14,15 @@ def coeffDM(trs, pts):
         Kp = np.abs(detJp)/2
         Bp = np.array([[yp[2]-yp[0], yp[0]-yp[1]],
                       [xp[0]-xp[2], xp[1]-xp[0]]])/detJp
-        liste_coeffM[tr, :, :] = (
-            Kp/12)*np.array([[2, 1, 1], [1, 2, 1], [1, 1, 2]])
-        liste_coeffD[tr, :, :] = Kp*Dphi@Bp.T@Bp@Dphi.T
-    return liste_coeffD, liste_coeffM
+        liste_Dp[tr, :, :] = Kp*Dphi@Bp.T@Bp@Dphi.T
+        liste_Kp[tr] = Kp
+    return liste_Dp, liste_Kp
 
 
 def solve_edp(triangles, points, labels, a, f, cond_lim):
 
     print('Génération des listes')
-    liste_coeffD, liste_coeffM = coeffDM(triangles, points)
+    liste_Dp, liste_Kp = coeffDK(triangles, points)
 
     print('Génération des matrices de masse et de rigidité')
     index_k = np.zeros(labels.shape[0], dtype=np.int64)
@@ -35,22 +34,24 @@ def solve_edp(triangles, points, labels, a, f, cond_lim):
     valeur = []
     for n_tri in range(triangles.shape[0]):
         triangle = triangles[n_tri]
-        ordre_tri = 0
+        x = np.sum(points[triangle, 0])/3
+        y = np.sum(points[triangle, 1])/3
+        a_xy = a(x, y)
+        f_xy = f(x, y)
+        Dp = liste_Dp[n_tri, :, :]
+        Kp = liste_Kp[n_tri]
+        s_tri = 0
         for noeud in triangle:
-            k = index_k[noeud]
             if labels[noeud] == 0:
-                B[k] = B[k] + f*liste_coeffM[n_tri, ordre_tri, ordre_tri]
+                k = index_k[noeud]
+                B[k] = B[k] + f_xy*Kp/3
                 ligne.append(k)
                 colonne.append(k)
-                valeur.append(liste_coeffD[n_tri, ordre_tri, ordre_tri] +
-                              a*liste_coeffM[n_tri, ordre_tri, ordre_tri])
-                for n_vois in [-1, 1]:
-                    noeud_voisin = triangle[(ordre_tri+n_vois) % 3]
+                valeur.append(Dp[s_tri, s_tri] + a_xy*Kp/6)
+                for s_vois in [-1, 1]:
+                    noeud_voisin = triangle[(s_tri+s_vois) % 3]
                     k_vois = index_k[noeud_voisin]
-                    val = liste_coeffD[n_tri, ordre_tri, (
-                        ordre_tri+n_vois) % 3]+a*liste_coeffM[n_tri, ordre_tri, (ordre_tri+n_vois) % 3]
-                    B[k] = B[k] + f * \
-                        liste_coeffM[n_tri, ordre_tri, (ordre_tri+n_vois) % 3]
+                    val = Dp[s_tri, (s_tri+s_vois) % 3] + a_xy*Kp/12
                     if labels[noeud_voisin] == 0:
                         ligne.append(k)
                         colonne.append(k_vois)
@@ -58,7 +59,7 @@ def solve_edp(triangles, points, labels, a, f, cond_lim):
                     for clim in range(len(cond_lim)):
                         if labels[noeud_voisin] == cond_lim[clim][0]:
                             B[k] = B[k] - cond_lim[clim][1]*val
-            ordre_tri = ordre_tri+1
+            s_tri = s_tri+1
     A = csr_array((valeur, (ligne, colonne)),
                   shape=(len(inconnus), len(inconnus)))
 
